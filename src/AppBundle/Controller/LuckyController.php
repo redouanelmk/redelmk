0190a91bb601622a19fc37d16e151b3326a2467f
redouane <elmouktafi.red@hotmail.fr>
2017-09-19 22:09:40 +0200

    creation d'une nouvelle vue

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,12 +1,15 @@
 <?php
 
 // src/AppBundle/Controller/LuckyController.php
+
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
@@ -15,8 +18,8 @@ class LuckyController
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
